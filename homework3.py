34910c48d1b80d480fddcdd7c68d559dd7d338d2
Olga5515 <torfenita@mail.ru>
2024-06-03 02:55:18 +1000

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+name = 'Olga'
+print('Name:', name)
+age = 50
+print('Age:', age)
+new = 2
+age = age + new
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
+# проверяю переменную на действительное логическое значение
+print(type(is_student))
+# отправлено 2024/06/03 02:55 по теме "Динамическая типизация"
